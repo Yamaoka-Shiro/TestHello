5aa8c65a91db7aeab1c505568e8db6addf657e3c
Yamaoka Shiro <yamaokashiro@gmail.com>
2012-08-30 22:21:44 +0900

    First commit

---
+++ TestHello/src/jp/ne/est_inc/android/exam/TestHelloActivity.java
@@ -0,0 +1,13 @@
+package jp.ne.est_inc.android.exam;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestHelloActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
